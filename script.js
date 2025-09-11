fef9d2ba24b80fc5c2b965828bb420989fb3770d
Benjamin Oliver Blanke <be.blanke@protonmail.com>
2025-09-11 15:33:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  menu.classList.toggle("show");
+}
+
 function updateCountdown() {
   const launchDate = new Date("2025-12-08T00:00:00");
   const now = new Date();
